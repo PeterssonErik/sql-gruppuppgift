080cd7c676327f7c986c1873e4a5bcfa2186f4fe
peterssonerik <erik__petersson@hotmail.com>
2016-03-30 15:22:18 +0200

    update

--- CLRDB/something.cs
+++ CLRDB/something.cs
@@ -11,7 +11,7 @@ public partial class StoredProcedures
     {
         SqlConnection conn = new SqlConnection();
         conn.ConnectionString = "Context Connection=true";
-
+        //lol
         SqlCommand cmd = new SqlCommand();
         cmd.Connection = conn;
         conn.Open();
